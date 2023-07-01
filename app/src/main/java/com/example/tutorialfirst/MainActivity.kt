3814367f1c5016de0560fbfbe42cf8a7aeb30bb8
Shivansh Kumar <Shivansh kumar>
2023-07-01 16:52:46 +0530

    Read Value

--- app/src/main/java/com/example/tutorialfirst/MainActivity.kt
+++ app/src/main/java/com/example/tutorialfirst/MainActivity.kt
@@ -2,19 +2,25 @@ package com.example.tutorialfirst
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var firebaseDatabase: FirebaseDatabase
     private lateinit var firebaseReference: DatabaseReference
 
-    lateinit var button :Button
+    lateinit var button: Button
     lateinit var editText: EditText
+    lateinit var Text: TextView
+    lateinit var Button2: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,21 +29,45 @@ class MainActivity : AppCompatActivity() {
         /**
          * This code helps to set value to the database
          */
-        firebaseDatabase = FirebaseDatabase.getInstance() // This helps in getting the INSTANCE to whole database
-        firebaseReference = firebaseDatabase.reference  // This helps in getting  the reference to the root node
+        firebaseDatabase =
+            FirebaseDatabase.getInstance() // This helps in getting the INSTANCE to whole database
+        firebaseReference =
+            firebaseDatabase.reference  // This helps in getting  the reference to the root node
 
         button = findViewById(R.id.button)
         editText = findViewById(R.id.editText)
+        Text = findViewById(R.id.Text)
+        Button2 = findViewById(R.id.button2)
 
-        button.setOnClickListener{
+        button.setOnClickListener {
             val name: String = editText.text.toString()
             firebaseReference.setValue(name)
 
         }
 
+        /**
+         * This code helps to read dats from the database
+         */
 
 
+        Button2.setOnClickListener {
 
+            firebaseReference.addListenerForSingleValueEvent(object : ValueEventListener {  // Getting the node from where I want to read data
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val data: String? = snapshot.getValue(String::class.java)
+                    Text.text = data
+
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Toast.makeText(applicationContext, "Something Went Wrong", Toast.LENGTH_SHORT)
+                        .show()
+
+                }
+
+            })
+
+        }
 
 
     }
